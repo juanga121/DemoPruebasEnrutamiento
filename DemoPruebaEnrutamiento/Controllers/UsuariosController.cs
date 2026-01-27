5769f629edffe068e06aa7a1267f70b394ee3e36
Juan Pablo <juan.giraldo@opitech.com.co>
2026-01-27 16:07:25 -0500

    ADD: Controllers e inyeccion de dependencias

---
+++ DemoPruebaEnrutamiento/Controllers/UsuariosController.cs
@@ -0,0 +1,51 @@
+﻿using DemoPruebasEnrutamiento.Application.Interface;
+using DemoPruebasEnrutamiento.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoPruebaEnrutamiento.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuariosController : Controller
+    {
+        private readonly IUsuarioService _service;
+
+        public UsuariosController(IUsuarioService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+            => Ok(await _service.ObtenerTodos());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var usuario = await _service.ObtenerPorId(id);
+            if (usuario == null) return NotFound();
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Usuario usuario)
+        {
+            await _service.Crear(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Usuario usuario)
+        {
+            await _service.Actualizar(usuario);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.Eliminar(id);
+            return NoContent();
+        }
+    }
+}
--- DemoPruebaEnrutamiento/Program.cs
+++ DemoPruebaEnrutamiento/Program.cs
@@ -1,3 +1,7 @@
+using DemoPruebasEnrutamiento.Application.Interface;
+using DemoPruebasEnrutamiento.Application.Services;
+using DemoPruebasEnrutamiento.Infraestructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
 var app = builder.Build();
 
